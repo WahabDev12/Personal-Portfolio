81723820737c71a8757797f248e4aa0af799038b
WahabDev12 <nerdyshinobi12@gmail.com>
2022-07-16 21:03:00 +0000

    minor changes

--- main.js
+++ main.js
@@ -37,6 +37,23 @@ document.querySelector(".btnMode").addEventListener("click", () => {
       document.querySelector(".btnMode").innerHTML = "Light 🌞";
     }
   });
+
+  // ANIMATED BACKGROUND
+
+  // window.addEventListener('DOMContentLoaded', () => {
+  //   VANTA.WAVES({
+  //     el: "#vanta",
+  //     mouseControls: true,
+  //     touchControls: true,
+  //     gyroControls: false,
+  //     minHeight: 200.00,
+  //     minWidth: 200.00,
+  //     scale: 1.00,
+  //     scaleMobile: 1.00,
+  //     color:"#292B43"
+      
+  //   })
+  // })
   
 
 
