a2178950a2d8f4b2e226eb6e2f52910cee694419
urdadx <70736338+urdadx@users.noreply.github.com>
2023-12-28 11:42:39 +0000

    Update presentation.ts
    removed linkedin

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -19,7 +19,7 @@ const presentation: Presentation = {
   mail: "abassabdulwahab3@gmail.com",
   title: "Hi, I'm Abdul Wahab 👋",
   description:
-    "Hi there, I'm Abdul Wahab, a *fullstack developer* and *Linux nerd 🐧*. I'm currently a Computer Science student at Ashesi University. I love *building cool stuff* and exploring *low-level languages*. Outside coding, I like to play video games and football ⚽ .",
+    "Hi there, I'm Abdul Wahab, a *fullstack developer* and *Linux fanboy 🐧*. I'm currently a Computer Science student at Ashesi University. I love *building cool stuff* and exploring *low-level languages*. Outside coding, I like to play video games and football ⚽ .",
   socials: [
     {
       label: "Twitter",
