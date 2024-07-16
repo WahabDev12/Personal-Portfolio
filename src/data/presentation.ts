1313b76b7a08c9316abf1f656c5ec241251811fa
urdad <70736338+urdadx@users.noreply.github.com>
2024-07-16 15:05:23 +0000

    update spacely link

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -16,63 +16,60 @@ type Presentation = {
 };
 
 const presentation: Presentation = {
-  mail: "abassabdulwahab3@gmail.com",
+  mail: 'abassabdulwahab3@gmail.com',
   title: "Hi, I'm Abdul Wahab 👋",
   description:
     "Hi there, I'm Abdul Wahab, a *fullstack developer* and *Linux nerd 🐧*. I'm currently a Computer Science student at Ashesi University. I love *building cool stuff* and exploring *low-level languages*. Outside coding, I like to play video games and football ⚽ .",
   socials: [
     {
-      label: "Twitter",
-      link: "https://twitter.com/NerdyProgramme2",
+      label: 'Twitter',
+      link: 'https://twitter.com/NerdyProgramme2',
     },
     {
-      label: "Github",
-      link: "https://github.com/urdadx",
+      label: 'Github',
+      link: 'https://github.com/urdadx',
     },
     {
-      label: "LinkedIn",
-      link: "https://linkedin.com/in/abdul-abass",
+      label: 'LinkedIn',
+      link: 'https://linkedin.com/in/abdul-abass',
     },
     {
-      label: "Book Me",
-      link: "https://cal.com/urdadx",
+      label: 'Book Me',
+      link: 'https://cal.com/urdadx',
     },
   ],
-  stacks:[
-     {
-        label: "NextJS",
-        link: "https://img.icons8.com/fluency/35/nextjs.png"
-     },
-     {
-        label: "TypeScript",
-        link: "https://img.icons8.com/fluency/35/typescript--v2.png"
-     },
-     {
-        label: "Python",
-        link: "https://img.icons8.com/fluency/35/python.png"
-     },
+  stacks: [
+    {
+      label: 'NextJS',
+      link: 'https://img.icons8.com/fluency/35/nextjs.png',
+    },
+    {
+      label: 'React Native',
+      link: 'https://img.icons8.com/color/35/expo.png',
+    },
+    {
+      label: 'Python',
+      link: 'https://img.icons8.com/fluency/35/python.png',
+    },
 
-     {
-        label: "NodeJS",
-        link: "https://img.icons8.com/color/35/nodejs.png"
-     },
-     {
-        label: "PostgreSQL",
-        link: "https://img.icons8.com/color/32/postgreesql.png"
-     },
-     {
-        label: "C++",
-        link: "https://img.icons8.com/color/35/c-plus-plus-logo.png"
-     },
-     
-     {
-        label: "Blender",
-        link: "https://img.icons8.com/fluency/35/blender-3d.png"
-     },
-     
+    {
+      label: 'Node',
+      link: 'https://img.icons8.com/color/35/nodejs.png',
+    },
+    {
+      label: 'Postgres',
+      link: 'https://img.icons8.com/color/32/postgreesql.png',
+    },
+    {
+      label: 'C++',
+      link: 'https://img.icons8.com/color/35/c-plus-plus-logo.png',
+    },
 
-     
-  ]
+    {
+      label: 'Blender',
+      link: 'https://img.icons8.com/fluency/35/blender-3d.png',
+    },
+  ],
 };
 
 export default presentation;
