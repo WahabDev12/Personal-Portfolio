03fa9d82143142d6128426a9e66517c2994dcd49
urdadx <nerdyshinobi12@gmail.com>
2024-09-23 10:17:10 +0000

    minor change

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,25 +7,25 @@ export type Project = {
 
 const projects: Project[] = [
   {
-    title: 'Librelinks / An opensource link in bio tool 🚀',
-    link: 'https://librelinks.vercel.app',
-    techs: ['ReactJS (NextJS)', 'MongoDB', 'Prisma'],
+    title: "Librelinks / An opensource link in bio tool 🚀",
+    link: "https://librelinks.vercel.app",
+    techs: ["NextJS", "MongoDB", "Prisma"],
   },
   {
-    title: 'Spacely / A meditation tool for studying ⚡️',
-    link: 'https://spacely.vercel.app/',
-    techs: ['ReactJS (NextJS)'],
+    title: "Spacely / A meditation tool for studying ⚡️",
+    link: "https://spacely.vercel.app/",
+    techs: ["ReactJS (NextJS)"],
   },
 
   {
-    title: 'Breeze / A screenshots editor for the web 🔥',
-    techs: ['ReactJS (Vite)'],
-    link: 'https://breeze-app.vercel.app/',
+    title: "Breeze / A screenshots editor for the web 🔥",
+    techs: ["ReactJS (Vite)"],
+    link: "https://breeze-app.vercel.app/",
   },
   {
-    title: 'Libre-gradients / A tool for customizing gradients ✨',
-    techs: ['ReactJS (NextJS)', 'TypeScript'],
-    link: 'https://libre-gradients.vercel.app',
+    title: "Libre-gradients / A tool for customizing gradients ✨",
+    techs: ["NextJS", "TypeScript"],
+    link: "https://libre-gradients.vercel.app",
   },
 ];
 
